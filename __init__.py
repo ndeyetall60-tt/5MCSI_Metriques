4c47f5b567b25872c3caddd094b93f6aad025e97
ndeyetall60-tt <ndeyetall60@gmail.com>
2025-12-03 11:45:58 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -31,5 +31,10 @@ def meteo():
 def mongraphique():
     return render_template("graphique.html")
 
+@app.route('/histogramme/')
+def histogramme():
+    return render_template('histogramme.html')
+
+
 if __name__ == "__main__":
   app.run(debug=True)
